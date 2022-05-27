fedebed193837830c0d617e0e2eb6cb5d621a725
patrick alexandre <palexandre@live.com>
2022-05-27 20:14:05 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace sistema_que_acha_a_maior_nota_em_um_array_de_tamanho_10
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            double[] array = new double[10];
+            double MaiorNumero = 0;
+
+
+           MaiorNumero =  maiorNumero( MaiorNumero,array);
+
+
+
+            Console.WriteLine("Seu maior Numero é: " + MaiorNumero);
+
+
+        }
+        static double maiorNumero(double MaiorNumero, double[]array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine("Digite 10 numeros");
+                array[i] = Convert.ToDouble(Console.ReadLine());
+
+                if (array[i] > MaiorNumero)
+                {
+                    MaiorNumero = array[i];
+
+                }
+
+
+
+
+            }
+            return MaiorNumero;
+        }
+        
+    }
+}
